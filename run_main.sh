95561c8b73dbcb7184f549ae8333a714760fe478
Kishan-Ved <kishan.ved@iitgn.ac.in>
2025-01-22 16:51:13 +0530

    added PROTEINS

--- run_main.sh
+++ run_main.sh
@@ -3,7 +3,7 @@ cluster="cluster"
 extra="extra"
 gradient_method="GD"
 baseline="baseline"
-for dataset in cora chameleon AIDS ZINC_subset
+for dataset in cora chameleon PROTEINS ZINC_subset
 do
     for coarsening_method in variation_neighborhoods # kron algebraic_JC heavy_edge variation_edges variation_cliques
     do
@@ -23,7 +23,7 @@ do
 
                             # Eg. 2: To run FIT-GNN model
                             output_dir=$dataset$underscore$exp_setup$underscore$coarsening_ratio$underscore$coarsening_method$underscore$extra$underscore$lr$underscore$batch_size$underscore$gradient_method
-                            python main.py --dataset $dataset --output_dir $output_dir --exp_setup $exp_setup --extra_node --coarsening_method $coarsening_method --coarsening_ratio $coarsening_ratio  --normalize_features --lr $lr --runs 1 --loss_reduction $loss_reduction --batch_size $batch_size --gradient_method $gradient_method --epochs1 300 --epochs2 300
+                            python main.py --dataset $dataset --output_dir $output_dir --exp_setup $exp_setup --extra_node --coarsening_method $coarsening_method --coarsening_ratio $coarsening_ratio  --normalize_features --lr $lr --runs 20 --loss_reduction $loss_reduction --batch_size $batch_size --gradient_method $gradient_method --epochs1 300 --epochs2 300
                         done
                     done
                 done
