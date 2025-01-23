5e0004a5b200fd82e884f59df251fa25a3eb2338
Kishan-Ved <kishan.ved@iitgn.ac.in>
2025-01-23 19:01:43 +0530

    clean sh file

--- run_inference_baseline.sh
+++ run_inference_baseline.sh
@@ -7,11 +7,3 @@ python inference_baseline.py --experiment random --dataset chameleon --num_test_
 python inference_baseline.py --experiment random --dataset PROTEINS --num_test_samples 10 --path_b ./save/graph_cls/baseline/PROTEINS_Gs_train_2_Gs_infer_0.3_variation_neighborhoods_cluster_0.01_128_GD_baseline/ --model_name_b model.pt
 
 python inference_baseline.py --experiment random --dataset ZINC_subset --num_test_samples 10 --path_b ./save/graph_reg/baseline/ZINC_subset_Gs_train_2_Gs_infer_0.3_variation_neighborhoods_cluster_0.01_128_GD_baseline/ --model_name_b model.pt
-
-# python inference.py --experiment random --dataset PROTEINS --num_test_samples 10 --path_b ./save/graph_cls/baseline/PROTEINS_Gs_train_2_Gs_infer_0.3_variation_neighborhoods_cluster_0.01_128_GD_baseline/ --model_name_b model.pt --baseline
-
-# python inference.py --dataset PROTEINS --num_test_samples 1000 --coarsening_method variation_neighborhoods --coarsening_ratio 0.5 --experiment random --exp_setup Gc_train_2_Gc_infer --extra_node --path_gc ./save/graph_cls/PROTEINS_Gs_train_2_Gs_infer_0.3_variation_neighborhoods_extra_0.01_128_GD/ --model_name_gc model.pt --path_b ./save/graph_cls/baseline/PROTEINS_Gs_train_2_Gs_infer_0.3_variation_neighborhoods_cluster_0.01_128_GD_baseline/ --model_name_b model.pt --baseline
-
-# python inference.py --dataset AIDS --num_test_samples 2000 --coarsening_method variation_neighborhoods --coarsening_ratio 0.3 --experiment random --exp_setup Gc_train_2_Gc_infer --extra_node --path_gc ./save/graph_cls/AIDS_Gs_train_2_Gs_infer_0.3_variation_neighborhoods_extra_0.01_128_GD/ --model_name_gc model.pt --path_b ./save/graph_cls/baseline/AIDS_Gs_train_2_Gs_infer_0.3_variation_neighborhoods_cluster_0.01_128_GD_baseline/ --model_name_b model.pt --baseline
-
-# python inference.py --dataset ZINC_subset --num_test_samples 5000 --coarsening_method variation_neighborhoods --coarsening_ratio 0.1 --experiment random --exp_setup Gc_train_2_Gc_infer --extra_node --path_gc ./save/graph_reg/ZINC_subset_Gs_train_2_Gs_infer_0.3_variation_neighborhoods_extra_0.01_128_GD/ --model_name_gc model.pt --path_b ./save/graph_reg/baseline/ZINC_subset_Gs_train_2_Gs_infer_0.3_variation_neighborhoods_cluster_0.01_128_GD_baseline/ --model_name_b model.pt --baseline
